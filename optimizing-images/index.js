108de7c38e70f63c0ee8f4b3dff54aa531066584
William Flores <williamdasflores@Williams-MacBook-Air.local>
2026-01-02 19:49:35 -0300

    Optimizing dependencies

--- optimizing-images/index.js
+++ optimizing-images/index.js
@@ -1,1 +1,12 @@
-console.log("Hello World for optimzing-image")
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    return res.send('Hello from my optimizing-image!!')
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
