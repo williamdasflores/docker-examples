7d83c638e9d1759aa270b778d7587935596679e9
William Flores <williamdasflores@Williams-MacBook-Air.local>
2026-01-04 10:45:28 -0300

    React example with docker

---
+++ react-example/src/App.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          This is application BLUE!
+        </p>
+        <p> It is runnning on nginx! And it is great</p>
+
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
